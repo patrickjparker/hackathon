dfa4673d3015375df7c7824104927abf57d5e905
Quincy Wilcox <qgwilcox@gmail.com>
2022-11-12 15:55:52 -0700

    Add form to About/Profile page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Profile</h1>
+    <form action="">
+      <label for="name">Name: </label>
+      <input type="text" id="name"><br>
+      <label for="age">Age: </label>
+      <input type="text" id="age"><br>
+      <label for="sex">Sex: </label>
+      <input type="text" id="sex"><br>
+    </form>
+
+    <div>
+
+    </div>
   </div>
 </template>
+
+<script>
+
+</script>
