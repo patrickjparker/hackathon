6502d3a9ed0719ed3ed5f896554f9a40fc828c75
patrickjparker <patrickjpark@gmail.com>
2022-11-12 11:24:18 -0700

    First commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+:root {
+  --color-primary: #ff0000;
+  --color-secondary: #00ff00;
+  --color-dark: #000000;
+  --color-light: #ffffff;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ frontend/src/components/Module.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Module",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  border: var(--color-dark) 1px solid;
+}
+</style>
